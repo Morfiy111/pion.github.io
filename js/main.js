b8dc77e5bd71532287765da1efabd361d4cd9fc2
morfiy <edurnev@keaz.ru>
2019-07-24 14:00:36 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ function tree(target) {
     var ul = target.parentNode.getElementsByTagName("ul").item(0);
     ul.style.display = (ul.style.display == "block") ? "none": "block";
 }
+
+$(document).ready(function(){
+    $(".btn-mobile").click(function(){
+        $("#toggleSample2").collapse('toggle');
+    });
+});
